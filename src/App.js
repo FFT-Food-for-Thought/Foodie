31414d3e046c823782a05520553f764ebce993fd
WhizBang <Kvance247@gmail.com>
2022-11-23 12:01:42 -0600

    added in Navbar and Home

--- src/App.js
+++ src/App.js
@@ -1,5 +1,32 @@
+import Home from "./pages/Home";
+// import "./style.scss";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+
 function App() {
-  return <div className="App"></div>;
+  return (
+    <Router>
+      <Routes>
+        <Route path="/">
+          <Route
+            path=""
+            index
+            element={
+              // <ProtectedRoute>
+              <Home />
+              // </ProtectedRoute>
+            }
+          />
+          {/* <Route path="login" element={<Login />} />
+          <Route path="register" element={<Register />} /> */}
+        </Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
