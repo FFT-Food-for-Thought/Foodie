86e8513b9252c830ad3611b080c11cfd297889d0
WhizBang <Kvance247@gmail.com>
2022-11-23 14:19:09 -0600

    passing on

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
 import db from "./db/firebase";
+import Navbar from "./Components/Navbar";
 // import { colRef } from "./db/firebase";
 // console.log(colRef);
 function App() {
-  return <div className="App">TEST</div>;
+  return (
+    <div className="App">
+      <Navbar />
+    </div>
+  );
 }
 
 export default App;
--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,16 +1,23 @@
 import React from "react";
 import "../Css/navbar.css";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
   return (
-    <div>
-      <div className="navbar-logo">
-        <div className="navbar-container">
-          <a href="/news">Foodie</a>
-          <a href="/news">About</a>
-          <a href="/news">Pop-Ups</a>
-          <a href="/news">Safety</a>
+    <div className="navbar-container">
+      <div className="navbar">
+        <div className="navbar-left">
+          <a href="/home">Foodie</a>
+
+          <a href="/about">About</a>
+
+          <a href="/popup">Pop-Ups</a>
+
+          <a href="/safety">Safety</a>
         </div>
+      </div>
+      <div class="navbar-right">
+        <div>Login</div>
       </div>
     </div>
   );
