a201c69a0dc41d3e3bf31b45882f793f67cc8f4c
robert.abril <robert.abril@gmail.com>
2022-11-27 17:02:49 -0500

    ignore

---
+++ src/Components/Login.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../Css/navbar.css';
+
+const Login = () => {
+  return (
+    <button type="button" onClick={console.log('test Login button')}>
+      Login
+    </button>
+  );
+};
+
+export default Login;
