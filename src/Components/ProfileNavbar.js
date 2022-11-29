326799f2f8736421506af96f35a76f8e013e6440
robert.abril <robert.abril@gmail.com>
2022-11-29 17:27:37 -0500

    After Login and Signup User is redirected to /Profile

--- src/Components/ProfileNavbar.js
+++ src/Components/ProfileNavbar.js
@@ -1,8 +1,18 @@
 import React, { useState } from "react";
 import "../Css/profile.css";
 import AddPhoto from "./AddPhoto";
+import { logout } from "../db/signup";
+import { useNavigate } from "react-router-dom";
+
 const ProfileNavbar = () => {
   const [isAddPhotoOpen, setAddPhotoIsOpen] = useState(false);
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    logout();
+    navigate("/");
+  };
+
   return (
     <div className="profile-navbar-container">
       <div>Profile Navbar</div>
@@ -10,6 +20,7 @@ const ProfileNavbar = () => {
         <button className="add-photo" onClick={() => setAddPhotoIsOpen(true)}>
           Add Photo
         </button>
+        <button onClick={handleLogout}>Logout</button>
 
         <AddPhoto
           openAddPhoto={isAddPhotoOpen}
