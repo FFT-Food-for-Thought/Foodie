cd7bb80801f7790feb7fe8a604dfd04357a81ba3
WhizBang <Kvance247@gmail.com>
2022-11-30 16:17:45 -0600

    toggle between messages and matches

--- src/Components/ProfileMatchMessage.js
+++ src/Components/ProfileMatchMessage.js
@@ -1,16 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import "../Css/profile.css";
 import SingleMatchView from "./SingleMatchView";
 const ProfileMatchMessage = ({ likedUsers }) => {
   const dummyData = ["Carina'sPFP, Kyle'PFP, PeterPFP, RobertPFP, THATGUYPFP"];
-  return (
-    <div className="matches">
-      <div>
-        {dummyData.map((profilePic) => {
-          return <SingleMatchView profilePic={profilePic} />;
-        })}
+  const [match, setMatch] = useState(true);
+  const [message, setMessage] = useState(false);
+
+  const toggleMatches = (e) => {
+    e.preventDefault();
+    setMessage(false);
+    setMatch(true);
+  };
+  const toggleMessasges = (e) => {
+    e.preventDefault();
+    setMessage(true);
+    setMatch(false);
+  };
+  if (match)
+    return (
+      <div className="matches">
+        <button onClick={toggleMatches}>Matches</button>
+        <button onClick={toggleMessasges}>Messages</button>
+
+        <div>
+          {dummyData.map((profilePic) => {
+            return <SingleMatchView profilePic={profilePic} />;
+          })}
+        </div>
       </div>
-      <div>Message Section</div>
+    );
+  return (
+    <div>
+      <button onClick={toggleMatches}>Matches</button>
+      <button onClick={toggleMessasges}>Messages</button>
+      <div>Hello</div>
     </div>
   );
 };
