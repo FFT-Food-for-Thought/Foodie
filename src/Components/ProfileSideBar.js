64587192b36858c2a829dd827b0daac028c7c51e
Peter <peterng.918@gmail.com>
2022-11-30 16:48:32 -0500

    added match components

--- src/Components/ProfileSideBar.js
+++ src/Components/ProfileSideBar.js
@@ -2,11 +2,11 @@ import React from "react";
 import ProfileMatchMessage from "./ProfileMatchMessage";
 import ProfileNavbar from "./ProfileNavbar";
 import "../Css/profile.css";
-const ProfileSideBar = () => {
+const ProfileSideBar = ({ likedUsers }) => {
   return (
     <>
       <ProfileNavbar />
-      <ProfileMatchMessage />
+      <ProfileMatchMessage likedUsers={likedUsers} />
     </>
   );
 };
