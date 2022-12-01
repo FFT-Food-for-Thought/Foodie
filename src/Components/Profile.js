e02c3ca99102d3b21cf01cc11680cf31a7c868ca
WhizBang <Kvance247@gmail.com>
2022-12-01 13:59:52 -0600

    profile view

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import ProfileSideBar from "./ProfileSideBar";
 import SingleProfileCard from "./SingleProfileCard";
+
 import "../Css/profile.css";
 import { onAuthStateChanged } from "firebase/auth";
 import { auth } from "../db/signup";
@@ -9,6 +10,7 @@ import OtherUserCards from "./OtherUserCards";
 
 const Profile = () => {
   const [user, setUser] = useState({});
+  const [isSingleView, setSingleViewClicked] = useState(false);
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, async (user) => {
@@ -22,15 +24,33 @@ const Profile = () => {
   console.log(user);
 
   console.log(user.pictureBucket);
-
+  if (isSingleView) {
+    return (
+      <>
+        <div className="sidebar">
+          <ProfileSideBar
+            likedUsers={user.likedUsers}
+            setSingleViewClicked={setSingleViewClicked}
+          />
+        </div>
+        <div className="picture-view">
+          <div className="box">
+            <SingleProfileCard user={user} />
+          </div>
+        </div>
+      </>
+    );
+  }
   return (
     <>
       <div className="sidebar">
-        <ProfileSideBar likedUsers={user.likedUsers} />
+        <ProfileSideBar
+          likedUsers={user.likedUsers}
+          setSingleViewClicked={setSingleViewClicked}
+        />
       </div>
       <div className="picture-view">
         <div className="box">
-          {/* <SingleProfileCard user={user} /> */}
           <OtherUserCards />
         </div>
       </div>
