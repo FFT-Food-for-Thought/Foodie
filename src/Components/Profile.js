83423620f6a9db763180a5efd7f83fc128c07c85
Peter <peterng.918@gmail.com>
2022-12-02 21:37:46 -0500

    will calculate your direct distance to disneyland if you allow location

--- src/Components/Profile.js
+++ src/Components/Profile.js
@@ -8,7 +8,7 @@ import { auth } from "../db/signup";
 import { getLoggedUser } from "../db/users";
 import OtherUserCards from "./OtherUserCards";
 import AllPhotos from "./AllPhotos";
-
+import { distance } from "../db/users";
 const Profile = () => {
   const [user, setUser] = useState({});
   const [isSingleView, setSingleViewClicked] = useState(false);
@@ -16,6 +16,33 @@ const Profile = () => {
     e.preventDefault();
     setSingleViewClicked(false);
   };
+  if ("geolocation" in navigator) {
+    /* geolocation is available */
+    console.log("gelocation is useable");
+    navigator.geolocation.getCurrentPosition(
+      (position) => {
+        console.log(position.coords.latitude, position.coords.longitude);
+
+        console.log(
+          "distance formulat being used, you are this many miles from disneyland in a straight line",
+          distance(
+            position.coords.latitude,
+            position.coords.longitude,
+            28.377242637128813,
+            -81.57071111805162
+          )
+        );
+      },
+      () => {
+        console.log(
+          "I can't calculate your distance from disney land if you don't share your location :("
+        );
+      }
+    );
+  } else {
+    /* geolocation IS NOT available */
+    //28.377242637128813, -81.57071111805162
+  }
 
   useEffect(() => {
     const unsub = onAuthStateChanged(auth, async (user) => {
