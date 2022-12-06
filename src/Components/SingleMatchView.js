83f2467d67c8f7e3716b95509dd5d9448bbf8bab
Peter <peterng.918@gmail.com>
2022-12-05 22:18:42 -0500

    should no longer break if a liked user has no pfp

--- src/Components/SingleMatchView.js
+++ src/Components/SingleMatchView.js
@@ -3,12 +3,24 @@ import { removeLike } from "../db/users";
 const SingleMatchView = ({ likedObj }) => {
   console.log("singlematchview profilepic", likedObj);
 
-  return (
-    <>
-      <img src={likedObj.URL} alt="profilepic" />
-      <p>{likedObj.name}</p>
-    </>
-  );
+  if (likedObj.URL) {
+    return (
+      <>
+        <img src={likedObj.URL} alt="profilepic" />
+        <p>{likedObj.name}</p>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <img
+          src="https://cdn.pixabay.com/photo/2015/10/05/22/37/blank-profile-picture-973460_960_720.png"
+          alt="profilepic"
+        />
+        <p>{likedObj.name}</p>
+      </>
+    );
+  }
 };
 
 export default SingleMatchView;
